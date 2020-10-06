f10771e1ee380d210dc59d2ebd6252c004574ba8
Maiara <maiara@Maiaras-MacBook-Air.local>
2020-10-06 15:21:06 +0100

    Added balance test and implementation

---
+++ lib/oystercard.rb
@@ -0,0 +1,7 @@
+class Oystercard
+    attr_reader :balance
+    BALANCE = 0
+    def initialize(balance = BALANCE)
+        @balance = balance 
+    end 
+end     
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,9 @@
+require 'oystercard.rb'
+
+describe Oystercard do
+  it "Has a initial balance of 0" do
+      expect(subject.balance).to eq (0)
+  end
+    
+    
+end
