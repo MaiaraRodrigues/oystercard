67e28026a1a28091303c4046186019e514c41b3c
Maiara <maiara@Maiaras-MacBook-Air.local>
2020-10-06 15:48:07 +0100

    Added value and passed tests for top up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,4 +4,8 @@ class Oystercard
     def initialize(balance = BALANCE)
         @balance = balance 
     end 
+
+    def top_up(value)
+      @balance += value 
+    end 
 end     
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,10 @@ describe Oystercard do
   it "Has a initial balance of 0" do
       expect(subject.balance).to eq (0)
   end
-    
+   
+  it " Allows to top up card" do
+    subject.top_up(5)
+    expect(subject.balance).to eq (5)
+  end
     
 end
